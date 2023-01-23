ea65868d42132805909d5db468884f7184e342b0
Vladislav Voitov <dobrihlopez@gmail.com>
2023-01-23 23:36:54 +0300

    add an album mode, it is much clearer now to read the brief description of the movie after a long click on it

--- app/src/main/java/com/voitov/movieskinopoisk/ApiService.java
+++ app/src/main/java/com/voitov/movieskinopoisk/ApiService.java
@@ -5,7 +5,7 @@ import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 public interface ApiService {
-    @GET("/movie?field=rating.kp&search=5-10&sortField=votes.kp&sortType=-1&limit=20&token=75ZNPX3-R7P40DV-NEKX12T-Y8EQ0H9")
+    @GET("/movie?field=rating.kp&search=3-10&sortField=votes.kp&sortType=-1&limit=40&token=75ZNPX3-R7P40DV-NEKX12T-Y8EQ0H9")
     public Single<MovieResponse> loadMostPopularMovies(@Query("page") int page);
 
     @GET("/movie?token=75ZNPX3-R7P40DV-NEKX12T-Y8EQ0H9&field=id")
